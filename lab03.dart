f351a8a19a65374a58461942df5077bacc80c67a
basharqawasmi <khaledbashar308@gmail.com>
2022-08-28 13:13:53 +0300

    this is my first upload

--- lab03.dart
+++ lab03.dart
@@ -1,9 +1,9 @@
 void  main() {
   
-  List colors = ["red","black","yeloow","white","pink","blue"];
+  List colors = ["red","black","yeloow","white","pink","blue","aqwa"];
 
   for (var i = 0; i < colors.length; i++) {
-    if (colors[i].toString().contains("a")||colors[i].toString().contains("b")){
+    if (colors[i].toString().startsWith("a")||colors[i].toString().startsWith("b")){
     print(colors[i].toString().toUpperCase());
   }else
   print(colors[i]+">>> sorry the color dont start with a or b");
